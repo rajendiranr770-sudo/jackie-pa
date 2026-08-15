cfcd3223f580eafbbff45ebdfa2e6d66c6ac7df7
rajendiranr770-sudo <rajendiranr770@gmail.com>
2026-08-16 03:30:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'smartpa-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
+const CACHE_NAME = "finance-app-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./app.js",
+    "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
 });
